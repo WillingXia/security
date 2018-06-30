7d6992da8bcc699c7a83eb35150d54f0bea9bae6
willing <wchangjixia@sina.com>
2018-06-30 16:45:21 +0800

    增加注释

--- src/main/java/com/example/demo/config/security/BrowerSecurityConfig.java
+++ src/main/java/com/example/demo/config/security/BrowerSecurityConfig.java
@@ -15,18 +15,14 @@ public class BrowerSecurityConfig extends WebSecurityConfigurerAdapter {
     @Autowired
     protected void configureGlobal(AuthenticationManagerBuilder auth) throws Exception{
         auth.inMemoryAuthentication().
-                withUser("user").password("123456").roles("user");
+                withUser("user").password("123456").roles("user");//指定登陆用户的用户名和密码
     }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.formLogin().loginPage("/login")
-                .and().authorizeRequests().antMatchers("/login").permitAll()
-                .and().authorizeRequests().anyRequest().authenticated();
+        http.formLogin().loginPage("/login")//指定登陆页面及入口
+                .and().authorizeRequests().antMatchers("/login").permitAll()//登陆入口所有人可见
+                .and().authorizeRequests().anyRequest().authenticated();//其他所有页面必须登陆后才能使用
     }
 
-    @Bean
-    protected PasswordEncoder passwordEncoder(){
-        return new BCryptPasswordEncoder();
-    }
 }
